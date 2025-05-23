d3974b5ed8410e308744503edede506bacb145e1
rafalkarczewski <karczewski.rafal93@gmail.com>
2025-05-23 09:00:09 +0300

    Add a reference to NVIDIA's EDM2 implementation

--- image_experiments/geodesic.py
+++ image_experiments/geodesic.py
@@ -1,11 +1,6 @@
-# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# You should have received a copy of the license along with this
-# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
+# This code is based on https://github.com/NVlabs/edm2/blob/main/generate_images.py
 
-"""Generate random images using the given model."""
+"""Generate a spacetime geodesic using the given model."""
 
 import os
 import re
